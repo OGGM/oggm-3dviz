7858b295e08c30ac73661632d7305e31795ed88e
Patrick Schmitt <patrick.schmitt@uibk.ac.at>
2024-11-06 13:44:42 +0100

    change topo colorbar fontcolor to white

--- oggm_3dviz/tools/viz.py
+++ oggm_3dviz/tools/viz.py
@@ -242,6 +242,8 @@ class Glacier3DViz:
             kwargs['add_mesh_topo_args']['scalar_bar_args'].setdefault(
                 'fmt', '%.0f m')
             kwargs['add_mesh_topo_args']['scalar_bar_args'].setdefault(
+                'color', 'white')
+            kwargs['add_mesh_topo_args']['scalar_bar_args'].setdefault(
                 'position_x', 0.9)
             kwargs['add_mesh_topo_args']['scalar_bar_args'].setdefault(
                 'position_y', 0.3)
